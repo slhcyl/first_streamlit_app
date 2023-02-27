c680bbcb66ae21ab4a24de1c51181cb433ca9c41
slhcyl <126477986+slhcyl@users.noreply.github.com>
2023-02-27 08:21:40 -0500

    Update streamlit_app.py
    added another header

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
