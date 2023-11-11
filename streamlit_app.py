b9f09196c481253d6302122bfdc4adef21754702
slhcyl <126477986+slhcyl@users.noreply.github.com>
2023-11-10 21:51:41 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,12 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
 
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
+
+
 # #create the repeatable code block(called a function)
 # def get_gruityvice_data(this_fruit_choice):
 #    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
